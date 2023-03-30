d9ba78670362569d0b02d13666676fa781cbbf23
koles1ko <koles1ko@yandex.ru>
2023-03-30 12:17:55 +0300

    some fixes

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,5 +1,5 @@
 const svgStroke = {
-  items: document.querySelectorAll(".l-intro__item"),
+  items: document.querySelectorAll(".stroke-item"),
   strokeLength: 0,
   interval: null,
   hoverHandlerBind: null,
@@ -12,16 +12,16 @@ const svgStroke = {
   },
   leaveHandler({target}) {
     clearInterval(this.interval)
-    target.closest(".l-intro__item").querySelector("rect").classList.remove("hovered")
+    target.closest(".stroke-item").querySelector("rect").classList.remove("hovered")
   },
   hoverHandler({target}) {
     const _this = this
     this.strokeLength = 0
-    const rect = target.closest(".l-intro__item").querySelector("rect")
+    const rect = target.closest(".stroke-item").querySelector("rect")
     rect.classList.add("hovered")
     const r = rect.rx.baseVal.value
-    const w = rect.closest(".l-intro__item").clientWidth + 6
-    const h = rect.closest(".l-intro__item").clientHeight + 6
+    const w = rect.closest(".stroke-item").clientWidth + 6
+    const h = rect.closest(".stroke-item").clientHeight + 6
     const length = 2 * Math.PI * r + 2 * (w - 2 * r) + 2 * (h - 2 * r)
     rect.style.height = `${h}px`
     rect.style.width = `${w}px`
@@ -41,18 +41,42 @@ const svgStroke = {
     this.leaveHandlerBind = this.leaveHandler.bind(this)
     this.hoverHandlerBind = this.hoverHandler.bind(this)
     this.addHandlers()
-    
-    //   this.circles.forEach(circle => {
-    //     const percent = circle.getAttribute("data-percent")
-    //     const picture = circle.querySelector("circle.current")
-    //     const radius = picture.r.baseVal.value
-    //     const length = 2 * Math.PI * radius
-    //     picture.style.strokeDasharray = `${length} ${length}`
-    //     picture.style.strokeDashoffset = length - length * percent / 100
-    //   })
+  }
+}
+
+const videoHandler = {
+  videoWrapper: null,
+  introItems: null,
+  wrapper: null,
+  setVideoHeightBind: null,
+  setVideoHeight() {
+    console.log(this)
+    console.log(window.innerWidth)
+    const introRect = this.introItems.getBoundingClientRect()
+    const wrapperRect = this.wrapper.getBoundingClientRect()
+    let videoHeight = 0
+    console.log(wrapperRect.top)
+    console.log(introRect.top)
+    console.log(introRect.top - wrapperRect.top)
+    if (window.innerWidth < 921) {
+      videoHeight = introRect.top - wrapperRect.top
+    }  else {
+      videoHeight = introRect.bottom - wrapperRect.top
+    }
+    this.videoWrapper.style.height = `${videoHeight}px`
+    // const introRect = this.introItems.getBoundingClientRect()
+  },
+  init() {
+    this.videoWrapper = document.querySelector(".content__video")
+    this.introItems = document.querySelector(".l-intro__items")
+    this.wrapper = document.querySelector(".wrapper")
+    this.setVideoHeightBind = this.setVideoHeight.bind(this)
+    this.setVideoHeightBind()
+    window.addEventListener("resize", this.setVideoHeightBind)
   }
 }
 
 window.addEventListener("DOMContentLoaded", () => {
   svgStroke.init()
+  videoHandler.init()
 })
